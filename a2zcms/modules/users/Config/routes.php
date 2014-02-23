7c4a02d57494d94cf80af68da7f75e8e938d2582
stojan.kukrika <mrakodol@gmail.com>
2014-02-23 22:41:56 +0100

    delete adminmenu,home,install,menu and  users modules to fix folder names in git

--- a2zcms/modules/users/Config/routes.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$route['users/logout'] 	= "users/logout";
-$route['users/account']	= "users/account";
-$route['users/login'] 	= "users/login";
-$route['users/register'] 	= "users/register";
-
-?>
