2c5ac990a60f4f3a1eba8e20847d3c655a7a858d
stojan.kukrika <mrakodol@gmail.com>
2014-02-23 22:31:59 +0100

    small fix in user main menu

--- a2zcms/modules/menu/Views/menu.php
+++ a2zcms/modules/menu/Views/menu.php
@@ -9,25 +9,24 @@
           <a class="navbar-brand" href="<?php echo base_url(); ?>"><?=$sitename?></a>
         </div>
         <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-   		 <ul class="nav navbar-nav navbar-right">
-		
+   		 <ul class="nav navbar-nav navbar-right">		
            <?php echo $items; ?>
-         		<?php if($currentuser): ?>
-						<li>
-							<a class="dropdown-toggle" id="dLabel" role="button" data-toggle="dropdown" data-target="#" href="#">
-								Logged as <strong><?php echo $currentuser->username; ?></strong><b class="caret"></b>
-							</a>
-							<ul class="dropdown-menu" role="menu" aria-labelledby="dLabel">
-								<? if(isset($admin)) {?>
-									<li><a href="<?=base_url('pages/index'); ?>">Admin panel</a></li>
-									<li class="divider"></li>
-								<? }?>
-								<li><a href="<?=base_url('users/account'); ?>">Edit Account</a></li>
+     		<?php if($currentuser): ?>
+					<li class='dropdown'>
+						<a class="dropdown-toggle" id="dLabel" role="button" data-toggle="dropdown" data-target="#" href="#">
+							Logged as <strong><?php echo $currentuser->username; ?></strong><b class="caret"></b>
+						</a>
+						<ul class='dropdown-menu' role="menu" aria-labelledby="dLabel">
+							<? if(isset($admin)) {?>
+								<li><a href="<?=base_url('pages/index'); ?>">Admin panel</a></li>
 								<li class="divider"></li>
-								<li><a href="<?=base_url('users/logout'); ?>">Logout</a></li>
-							</ul>
-						</li>
-				<?php endif; ?>
+							<? }?>
+							<li><a href="<?=base_url('users/account'); ?>">Edit Account</a></li>
+							<li class="divider"></li>
+							<li><a href="<?=base_url('users/logout'); ?>">Logout</a></li>
+						</ul>
+					</li>
+			<?php endif; ?>
 				
 			</ul>
         </div><!--/.navbar-collapse -->
